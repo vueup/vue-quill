6dd839a7730535995b5f991e99da316a94a49706
Ahmad Luthfi Masruri <luthfimasruri@gmail.com>
2021-02-26 14:06:01 +0700

    Add tailwindcss

--- demo/src/main.ts
+++ demo/src/main.ts
@@ -1,7 +1,8 @@
 import { createApp } from "vue"
 import App from "./App.vue"
+import "./style.css"
 
-// import QuillEditor from "vue3-quill-editor" // when use npm
+// import QuillEditor from "@vueup/quill" // when use npm
 import QuillEditor from "../../src/main"
 
 createApp(App)

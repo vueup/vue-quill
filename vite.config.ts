b2aa19a412ee431b4e9dde32f7d090013e06cdb4
Ahmad Luthfi Masruri <luthfimasruri@gmail.com>
2021-02-26 14:06:33 +0700

    Clean up the code

--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,28 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import copy from 'rollup-plugin-copy'
-const path = require('path')
 
-const buildPluginConfig = defineConfig({
+const libBuildConfig = defineConfig({
   plugins: [
     vue()
   ],
   build: {
     lib: {
-      entry: path.resolve(__dirname, './src/main.ts'),
+      entry: './src/main.ts',
       name: 'QuillEditor',
       formats: ['es', 'cjs', 'umd', 'iife'],
     },
     rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
+      // Externalize deps that shouldn't be bundled into the library
       external: ['vue'],
       output: {
         exports: "named",
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          vue: 'Vue'
-        }
+        // Provide global variables to use in the UMD build for externalized deps
+        globals: { vue: 'Vue' }
       },
       plugins: [
         copy({
-          targets: [
-            { src: './node_modules/quill/dist/quill.core.css', dest: './dist' },
-            { src: './node_modules/quill/dist/quill.snow.css', dest: './dist' },
-            { src: './node_modules/quill/dist/quill.bubble.css', dest: './dist' }
-          ],
+          targets: [{ src: './node_modules/quill/dist/*.css', dest: './dist' }],
           hook: 'writeBundle',
           verbose: true,
           copyOnce: true
@@ -41,19 +32,18 @@ const buildPluginConfig = defineConfig({
   }
 })
 
-const buildDemoConfig = defineConfig({
+const demoBuildConfig = defineConfig({
   plugins: [
     vue()
   ],
+  publicDir: "./demo/public",
   base: "/vueup-quill/",
   build: {
-    outDir: path.resolve(__dirname, './demo/dist'),
+    outDir: './demo/dist',
     rollupOptions: {
       plugins: [
         copy({
-          targets: [
-            { src: './docs/.vitepress/dist/*', dest: './demo/dist/docs' },
-          ],
+          targets: [{ src: './docs/.vitepress/dist/*', dest: './demo/dist/docs' }],
           hook: 'writeBundle',
           verbose: true,
           copyOnce: true
@@ -66,15 +56,15 @@ const buildDemoConfig = defineConfig({
 const defaultConfig = defineConfig({
   plugins: [
     vue()
-  ]
+  ],
+  publicDir: "./demo/public",
 })
-
 
 export default ({ command, mode }) => {
   if (command === 'build' && mode === 'lib') {
-    return buildPluginConfig
+    return libBuildConfig
   } else if (command === 'build' && mode === 'demo') {
-    return buildDemoConfig
+    return demoBuildConfig
   } else {
     return defaultConfig
   }

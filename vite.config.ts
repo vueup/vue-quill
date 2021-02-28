9b9d8bf5d8cd5897c8c1a867fd9d1a5eb6714a12
Ahmad Luthfi Masruri <luthfimasruri@gmail.com>
2021-02-28 10:16:28 +0700

    Change config to object

--- vite.config.ts
+++ vite.config.ts
@@ -1,76 +1,78 @@
-import { defineConfig } from 'vite'
+import { defineConfig, UserConfigExport } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import copy from 'rollup-plugin-copy'
 const csso = require('csso');
 
-const libBuildConfig = defineConfig({
-  plugins: [
-    vue()
-  ],
+const config = {
   build: {
-    lib: {
-      entry: './src/main.ts',
-      name: 'QuillEditor',
-      formats: ['es', 'cjs', 'umd', 'iife'],
-    },
-    rollupOptions: {
-      // Externalize deps that shouldn't be bundled into the library
-      external: ['vue'],
-      output: {
-        exports: "named",
-        // Provide global variables to use in the UMD build for externalized deps
-        globals: { vue: 'Vue' }
-      },
+    lib: defineConfig({
       plugins: [
-        copy({
-          targets: [{
-            src: './node_modules/quill/dist/*.css',
-            dest: './dist',
-            transform: (contents, filename) => csso.minify(contents.toString()).css
-          }],
-          hook: 'writeBundle',
-          verbose: true,
-          copyOnce: true
-        })
-      ]
-    }
-  }
-})
-
-const demoBuildConfig = defineConfig({
-  plugins: [
-    vue()
-  ],
-  publicDir: "./demo/public",
-  base: "/vueup-quill/",
-  build: {
-    outDir: './demo/dist',
-    rollupOptions: {
+        vue()
+      ],
+      build: {
+        lib: {
+          entry: './src/main.ts',
+          name: 'QuillEditor',
+          formats: ['es', 'cjs', 'umd', 'iife'],
+        },
+        rollupOptions: {
+          // Externalize deps that shouldn't be bundled into the library
+          external: ['vue'],
+          output: {
+            exports: "named",
+            // Provide global variables to use in the UMD build for externalized deps
+            globals: { vue: 'Vue' }
+          },
+          plugins: [
+            copy({
+              targets: [{
+                src: './node_modules/quill/dist/*.css',
+                dest: './dist',
+                transform: (contents, filename) => csso.minify(contents.toString()).css
+              }],
+              hook: 'writeBundle',
+              verbose: true,
+              copyOnce: true
+            })
+          ]
+        }
+      }
+    }),
+    demo: defineConfig({
       plugins: [
-        copy({
-          targets: [{ src: './docs/.vitepress/dist/*', dest: './demo/dist/docs' }],
-          hook: 'writeBundle',
-          verbose: true,
-          copyOnce: true
-        })
-      ]
-    }
-  }
-})
+        vue()
+      ],
+      publicDir: "./demo/public",
+      base: "/vueup-quill/",
+      build: {
+        outDir: './demo/dist',
+        rollupOptions: {
+          plugins: [
+            copy({
+              targets: [{ src: './docs/.vitepress/dist/*', dest: './demo/dist/docs' }],
+              hook: 'writeBundle',
+              verbose: true,
+              copyOnce: true
+            })
+          ]
+        }
+      }
+    })
+  },
+  default: defineConfig({
+    plugins: [
+      vue()
+    ],
+    publicDir: "./demo/public",
+  })
+}
 
-const defaultConfig = defineConfig({
-  plugins: [
-    vue()
-  ],
-  publicDir: "./demo/public",
-})
-
-export default ({ command, mode }) => {
+export default ({ command, mode }): UserConfigExport => {
   if (command === 'build' && mode === 'lib') {
-    return libBuildConfig
+    return config.build.lib
   } else if (command === 'build' && mode === 'demo') {
-    return demoBuildConfig
+    return config.build.demo
   } else {
-    return defaultConfig
+    return config.default
   }
 }

5b974221c8bf904f96a4d4a59c3ee5784d096add
Ahmad Luthfi Masruri <luthfimasruri@gmail.com>
2021-02-28 10:20:16 +0700

    Add theme css

--- demo/src/main.ts
+++ demo/src/main.ts
@@ -1,9 +1,12 @@
 import { createApp } from "vue"
 import App from "./App.vue"
+import 'quill/dist/quill.core.css' // import styles
+import 'quill/dist/quill.snow.css' // for snow theme
+import 'quill/dist/quill.bubble.css' // for bubble theme
 import "./style.css"
 
 // import QuillEditor from "@vueup/quill" // when use npm
-import QuillEditor from "../../src/main"
+import QuillEditor from "../../dist/quill.es"
 
 createApp(App)
   .use(QuillEditor)

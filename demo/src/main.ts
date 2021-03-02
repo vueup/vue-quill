d73dd8a3ffe39ba843866f6768e5194e37b1b107
Ahmad Luthfi Masruri <luthfimasruri@gmail.com>
2021-03-02 09:09:43 +0700

    Add quill theme css style

--- demo/src/main.ts
+++ demo/src/main.ts
@@ -1,8 +1,10 @@
 import { createApp } from "vue"
 import App from "./App.vue"
-// import 'quill/dist/quill.core.css' // import styles
-// import 'quill/dist/quill.snow.css' // for snow theme
-// import 'quill/dist/quill.bubble.css' // for bubble theme
+
+import 'quill/dist/quill.core.css' // import styles
+import 'quill/dist/quill.snow.css' // for snow theme
+import 'quill/dist/quill.bubble.css' // for bubble theme
+
 import "./style.css"
 
 // import QuillEditor from "@vueup/quill" // when use npm

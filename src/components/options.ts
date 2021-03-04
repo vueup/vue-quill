9bf324f8374cd8fb34f4e1185704180e3219dc8b
Ahmad Luthfi Masruri <luthfimasruri@gmail.com>
2021-03-04 18:34:22 +0700

    Update toolbar options default -> essential

--- src/components/options.ts
+++ src/components/options.ts
@@ -1,5 +1,5 @@
 export const toolbarOptions = {
-  default: [
+  essential: [
     [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
     ['bold', 'italic', 'underline'],
     [{ 'list': 'ordered' }, { 'list': 'bullet' }],

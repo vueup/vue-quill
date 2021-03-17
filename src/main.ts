e64a30a059360d47783d9819198767b105c92566
Ahmad Luthfi Masruri <luthfimasruri@gmail.com>
2021-03-17 13:44:19 +0700

    BREAKING CHANGE: remove install functions

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,4 @@
 
 import QuillEditor from "./components/QuillEditor"
 import Quill from "quill"
-import { App } from "vue"
-
-// Declare install function executed by Vue.use()
-const install = (app: App<Element>, globalOptions: object) => {
-  if (globalOptions) {
-    QuillEditor.props.globalOptions.default = () => globalOptions
-  }
-  app.component(QuillEditor.name, QuillEditor)
-}
-
-export { install as default, QuillEditor, Quill };
+export { QuillEditor as default, Quill };

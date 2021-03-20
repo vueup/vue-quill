6ae47e4cdfc4b2bc2d2b1c6e97d4aa1ede999c49
Ahmad Luthfi Masruri <luthfimasruri@gmail.com>
2021-03-20 21:35:10 +0700

    chore: delete git cached

--- docs/utils/github-api.ts
+++
@@ -1,7 +0,0 @@
-export const getLatestReleaseVersion = async (owner: string, repo: string): Promise<string> => {
-  return await fetch(
-    `https://api.github.com/repos/${owner}/${repo}/releases/latest`
-  )
-    .then((response) => response.json())
-    .then((data) => data.tag_name.replace("v", ""));
-};
--- docs/utils/regex.ts
+++
@@ -1,3 +0,0 @@
-export const escapeRegExp = (string: string) => {
-  return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"); // $& means the whole matched string
-}

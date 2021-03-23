4c64c4c107673296892bf9f38b3769c2b2c75a24
Ahmad Luthfi Masruri <luthfimasruri@gmail.com>
2021-03-23 22:15:36 +0700

    docs: improve github-api utils

--- docs/utils/github-api.ts
+++ docs/utils/github-api.ts
@@ -3,20 +3,44 @@ export const getLatestReleaseVersion = async (owner: string, repo: string): Prom
     `https://api.github.com/repos/${owner}/${repo}/releases/latest`
   )
     .then((response) => response.json())
-    .then((data) => {
-      if (data.tag_name) {
-        return data.tag_name.replace("v", "")
+    .then((latestRelease) => {
+      if (latestRelease.tag_name) {
+        return latestRelease.tag_name.match(/(\d+\.\d+)[\-\.\da-zA-Z]+/)[0] ?? ""
       } else {
         return fetch(
           `https://api.github.com/repos/${owner}/${repo}/tags`
         )
           .then((response) => response.json())
-          .then((data) => {
-            try {
-              return data[0].name.replace("v", "")
-            } catch {
-              return "latest"
+          .then((tags: any[]) => {
+            if (tags.length) {
+              // get latest tag version
+              return tags[0].name.match(/(\d+\.\d+)[\-\.\da-zA-Z]+/)[0] ?? ""
             }
+            return ""
+          })
+      }
+    })
+};
+
+export const getLatestRelease = async (owner: string, repo: string): Promise<string> => {
+  return await fetch(
+    `https://api.github.com/repos/${owner}/${repo}/releases/latest`
+  )
+    .then((response) => response.json())
+    .then((latestRelease) => {
+      if (latestRelease.prerelease === false) {
+        return "latest"
+      } else {
+        return fetch(
+          `https://api.github.com/repos/${owner}/${repo}/tags`
+        )
+          .then((response) => response.json())
+          .then((tags: any[]) => {
+            if (tags.length) {
+              // get prerelease name (e.g. alpha) from the latest tag name
+              return tags[0].name.match(/(?<=\.\d-)[^.]+/)[0] ?? ""
+            }
+            return ""
           })
       }
     })

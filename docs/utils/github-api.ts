186e0256b99f97d64fce0716743e06a5cb8aee61
Ahmad Luthfi Masruri <luthfimasruri@gmail.com>
2021-03-23 14:15:53 +0700

    fix: add fallback github release to tags

--- docs/utils/github-api.ts
+++ docs/utils/github-api.ts
@@ -3,5 +3,21 @@ export const getLatestReleaseVersion = async (owner: string, repo: string): Prom
     `https://api.github.com/repos/${owner}/${repo}/releases/latest`
   )
     .then((response) => response.json())
-    .then((data) => data.tag_name.replace("v", ""));
+    .then((data) => {
+      if (data.tag_name) {
+        return data.tag_name.replace("v", "")
+      } else {
+        return fetch(
+          `https://api.github.com/repos/${owner}/${repo}/tags`
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            try {
+              return data[0].name.replace("v", "")
+            } catch {
+              return "latest"
+            }
+          })
+      }
+    })
 };

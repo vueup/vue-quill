ce33da04b6892c84d9c5b5844665dbdcd78342ec
Ahmad Luthfi Masruri <luthfimasruri@gmail.com>
2021-03-28 19:42:11 +0700

    fix: semantic release

--- scripts/semantic-release.js
+++ scripts/semantic-release.js
@@ -6,12 +6,17 @@ const args = require('minimist')(process.argv.slice(2))
 const targets = args._
 
 if (process.env.CI && targets[0]) {
+  const rootDir = path.resolve(__dirname, '..')
   const pkgDir = path.resolve(__dirname, '../packages', target)
   const target = targets[0]
   const distDir = path.resolve(pkgDir, 'dist')
   const semanticReleaseConfig = path.resolve(pkgDir, 'semantic-release.json')
-  execa('zip', [`${target}vue-quill-dist.zip`, '-r', distDir])
-  execa('npx', ['semantic-release', '--extends', semanticReleaseConfig])
+  execa.sync('cd', [pkgDir])
+  execa.sync('npm', ['ci'])
+  execa.sync('cd', [rootDir])
+  execa.sync('npm', ['ci'])
+  execa.sync('zip', [`${target}vue-quill-dist.zip`, '-r', distDir])
+  execa.sync('npx', ['semantic-release', '--extends', semanticReleaseConfig])
 } else {
   console.log(chalk.redBright("You can't run semantic-release locally"))
 }

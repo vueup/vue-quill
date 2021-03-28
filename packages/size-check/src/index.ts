ece8796bee5ed70f07ad41d48b4835da36b91c47
Ahmad Luthfi Masruri <luthfimasruri@gmail.com>
2021-03-28 23:58:27 +0700

    feat: add size-check package

---
+++ packages/size-check/src/index.ts
@@ -0,0 +1,9 @@
+import { h, createApp } from 'vue'
+import { QuillEditor } from '@vueup/vue-quill'
+
+// The bare minimum code required for rendering something to the screen
+const app = createApp({
+  render: () => h('div', 'hello world!')
+})
+app.component('Editor', QuillEditor)
+app.mount('#app')

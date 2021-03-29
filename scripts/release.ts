4f2808b160242bcd748069a010a434e8eab4a51d
Ahmad Luthfi Masruri <luthfimasruri@gmail.com>
2021-03-29 07:54:43 +0700

    fix: zip command

--- scripts/release.ts
+++ scripts/release.ts
@@ -6,7 +6,7 @@
   const targets = args._
 
   if (process.env.CI && targets[0]) {
-    execa.sync('zip', ['-r', `${targets[0]}-dist.zip`, '-i', 'dist'])
+    execa.sync('zip', ['-r', `${targets[0]}-dist.zip`, '.', '-i', 'dist'])
     execa.sync('npx', ['semantic-release'])
   } else {
     console.log(chalk.redBright("You can't run semantic-release locally"))

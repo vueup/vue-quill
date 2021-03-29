b13af171768a2ca85a443d3519fe108e84ab5fb0
Ahmad Luthfi Masruri <luthfimasruri@gmail.com>
2021-03-29 15:52:25 +0700

    chore:

--- .debug/src/main.ts
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import "@vueup/quill/dist/quill.core.css"
-import "@vueup/quill/dist/quill.snow.css"
-import "@vueup/quill/dist/quill.bubble.css"
-
-createApp(App).mount('#app')

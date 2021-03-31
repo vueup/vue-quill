aa762c1c020172b97cd006ea8267c656bfc30512
Ahmad Luthfi Masruri <luthfimasruri@gmail.com>
2021-03-31 19:34:00 +0700

    build: add eslint-config-prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:vue/vue3-recommended',
     'plugin:@typescript-eslint/recommended',
+    'prettier',
   ],
   parserOptions: {
     ecmaVersion: 12,

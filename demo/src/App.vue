c00844e2cfaa8ca066bc83b001f0bb787f0c169e
Ahmad Luthfi Masruri <luthfimasruri@gmail.com>
2021-04-13 04:04:56 +0700

    chore: :construction: add demo editor

---
+++ demo/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <BasicExample />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import BasicExample from './components/BasicExample.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    BasicExample,
+  },
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
---
+++ demo/src/components/BasicExample.vue
@@ -0,0 +1,37 @@
+<template>
+  <h1>BasicEditor</h1>
+  <QuillEditor v-model:content="contentDelta" contentType="delta" />
+  <pre>{{ contentDelta }}</pre>
+  <QuillEditor v-model:content="contentHTML" contentType="html" />
+  <pre>{{ contentHTML }}</pre>
+  <QuillEditor v-model:content="contentText" contentType="text" />
+  <pre>{{ contentText }}</pre>
+</template>
+
+<script lang="ts">
+import { ref, defineComponent } from 'vue'
+import { QuillEditor, Delta } from '@vueup/vue-quill'
+import '@vueup/vue-quill/../dist/vue-quill.snow.css'
+
+export default defineComponent({
+  components: {
+    QuillEditor,
+  },
+  setup: () => {
+    const contentDelta = ref<Delta>(
+      new Delta([
+        { insert: 'Gandalf', attributes: { bold: true } },
+        { insert: ' the ' },
+        { insert: 'Grey', attributes: { color: '#ccc' } },
+      ])
+    )
+    const contentHTML = ref('<h1>This is html header</h1>')
+    const contentText = ref('This is just plain text')
+    setTimeout(() => {
+      contentText.value = 'COOOOOOOK'
+    }, 3000)
+    return { contentDelta, contentHTML, contentText }
+  },
+})
+</script>
+

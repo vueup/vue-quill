417b1ccf5b97d370c84d0542097c1fb4c888791e
luthfimasruri <luthfimasruri@users.noreply.github.com>
2021-05-03 14:33:44 +0000

    deploy: 79ad2c5dd9d46c72439e439d188605e3a0874acd

---
+++ assets/api_slots.md.18adb56d.lean.js
@@ -0,0 +1,1 @@
+import{o as a,a as n,i as s}from"./app.53ab8df2.js";const t='{"title":"#toolbar","description":"","frontmatter":{},"relativePath":"api/slots.md","lastUpdated":1620052371316}',p={},o=s('',3);p.render=function(s,t,p,l,c,e){return a(),n("div",null,[o])};export default p;export{t as __pageData};
--- assets/guide_themes.md.1dc7e420.lean.js
+++
@@ -1,1 +0,0 @@
-import{o as e,a as s,i as a}from"./app.48ac182a.js";const t='{"title":"Themes","description":"","frontmatter":{},"relativePath":"guide/themes.md","lastUpdated":1619232551026}',n={},o=a('',7);n.render=function(a,t,n,p,l,c){return e(),s("div",null,[o])};export default n;export{t as __pageData};
